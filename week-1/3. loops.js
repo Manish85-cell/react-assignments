bb368c6cc6ed7d570b91587d47826ccd5dc65ce2
Manish85-cell <114242719+Manish85-cell@users.noreply.github.com>
2023-09-16 08:54:34 +0530

    Update 3. loops.js

--- week-1/3. loops.js
+++ week-1/3. loops.js
@@ -17,3 +17,76 @@ Implement a program that calculates and displays the factorial of a given number
 Prime Number Checker:
 Create a function that checks whether a given number is prime or not. Use a loop to test divisibility by numbers from 2 to the square root of the given number.
  */
+function count()
+{
+ let i = 10;
+ while(i>0)
+  { 
+    console.log(i);
+    i--;
+  }
+ console.log("Blast Off!");
+}
+count();
+function evensum(n){
+ let sum = 0;
+   for(let i = 2; i <=n;i++){
+       if(i%2 == 0)
+       {
+        sum = sum + i;
+       }
+   }
+ return sum;
+}
+console.log(evensum(50));
+function table(n)
+{
+ let i = 1;
+ while(i<=10)
+  {
+   console.log(n+"*"+i +"="+i*n);
+   i++;
+  }
+}
+table(5);
+function reverse(s)
+{
+    let rev = "";
+    for(let i = s.length-1; i>=0; i--)
+    {
+        rev = rev+s.charAt(i);
+    }
+    return rev;
+}
+console.log(reverse("hsinaM"));
+function reverse(s)
+{
+ let low = 0;
+ let high = s.length;
+ while(low<high)
+  {
+   
+  }
+}
+function factorial(n){
+    let fac=n;
+     while(n>1)
+      {
+       fac=fac*(n-1);
+       n--;
+      }
+      return fac;
+ }
+ console.log(factorial(5));
+
+function isprime(n) {
+    for (let i = 2; i!=n && i < 10; i++) {
+        if (n % i == 0) {
+            console.log(n+" Not prime");
+            return;
+        }
+    }
+    console.log(n+" is prime");
+}
+
+isprime(11);
